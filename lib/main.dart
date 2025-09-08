e7da068606d0a3984bbbca55e95e273dfacf81de
nadao-1707 <princeegypt28@gmail.com>
2025-09-08 13:46:38 +0300

    made the login screen ui

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:movieapp/login_screen.dart';
 import 'package:movieapp/register_screen.dart';
 
 void main() {
-  runApp( movieApp());
+  runApp(movieApp());
 }
 
 class movieApp extends StatelessWidget {
-  
+  const movieApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {RegisterScreen.routeName: (_) => RegisterScreen()},
-      initialRoute: RegisterScreen.routeName,
+    return ScreenUtilInit(
+      designSize: const Size(430, 932),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        routes: {
+          RegisterScreen.routeName: (_) => RegisterScreen(),
+          LoginScreen.routeName: (context) => LoginScreen(),
+        },
+        initialRoute: LoginScreen.routeName,
+      ),
     );
   }
 }
